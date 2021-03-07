d82e5f19c49552a21007c5955c7d0abf64558849
jerateeps <jerateeps@thaicom.net>
2021-03-07 23:24:42 +0700

    addDetails

--- BirthdayCalculator/Program.cs
+++ BirthdayCalculator/Program.cs
@@ -6,20 +6,41 @@ namespace BirthdayCalculator
     {
         static void Main(string[] args)
         {
-            Console.Write("Input Birthday (DD-MM-YYYY): ");
+            Console.WriteLine("Welcome to your personal age calcualtor");
+            Console.WriteLine("Please provide me your details");
+            Console.Write("First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("City: ");
+            string city = Console.ReadLine();
+            Console.Write("Country: ");
+            string country = Console.ReadLine();           
+            Console.Write("Dae for birth (DD-MM-YYYY): ");
             DateTime Birthday = DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", null);
-            int Days = DaysPassedSinceYourBirth(Birthday);
-            double Hours = HoursPassedSinceYourBirth(Birthday);
-            double Minutes = MinutesPassedSinceYourBirth(Birthday);
-            double Seconds = SecondsPassedSinceYourBirth(Birthday);
-            Console.WriteLine("{0} Day", Days);
-            Console.WriteLine("{0} Hours", Hours);
-            Console.WriteLine("{0} Minutes", Minutes);
-            Console.WriteLine("{0} Seconds", Seconds);
+            string Result = GetMyInformation(firstName, lastName, city,country, Birthday);
+            Console.WriteLine(Result);
+            Console.Write("Press any key continue");
+            Console.ReadLine();
         }
         public static string GetMyInformation(string firstName, string lastName, string city, string country, DateTime dob)
         {
-            return "";
+            int Days = DaysPassedSinceYourBirth(dob);
+            double Hours = HoursPassedSinceYourBirth(dob);
+            double Minutes = MinutesPassedSinceYourBirth(dob);
+            double Seconds = SecondsPassedSinceYourBirth(dob);
+            Console.WriteLine("*** Hear are your details ***");
+            Console.WriteLine("Name: {0} {1}", firstName, lastName);
+            Console.WriteLine("  City: {0}", city);
+            Console.WriteLine("  Country: {0}", country);
+            Console.WriteLine("  Date of birth: {0}", dob);
+            Console.WriteLine("****************************");
+            Console.WriteLine("*** Here are your age numgers ***");
+            Console.WriteLine("Your age in day : {0}", Days);
+            Console.WriteLine("Your approximate age in horus: {0}", Hours);
+            Console.WriteLine("Your approximate age in minutes: {0}", Minutes);
+            Console.WriteLine("Your approximate age in seconds: {0}", Seconds);
+            return "***Thank You***";
         }
         public static int DaysPassedSinceYourBirth(DateTime dob)
         {
